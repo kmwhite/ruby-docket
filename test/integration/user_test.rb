d8579bfa49a61a6a78fa267e23660a4fce886ca2
Kristofer White <me@kmwhite.net>
2014-01-29 13:46:45 -0600

    Completing basic dashboard tests

--- test/integration/user_test.rb
+++ test/integration/user_test.rb
@@ -43,4 +43,33 @@ class Integration::UserTest < ActionDispatch::IntegrationTest
       click_button 'Cancel my account'
     end
   end
+
+  test 'Rendering the user dashboard' do
+    other_user = User.make!(name: 'Jane Doe', password: 'pass123worD!',
+                            team: @user.team)
+
+    completed_project = Project.make!(team: @user.team)
+    completed_project.complete!
+
+    inc_proj           = Project.make!(team: @user.team)
+    reported_task      = Task.make!(project: inc_proj, reporter: @user, owner: other_user)
+    owned_task         = Task.make!(project: inc_proj, reporter: other_user, owner: @user)
+
+    login_user(@user)
+
+    assert page.has_xpath?("//*[@id='db-projects-open']/div/a[@id='project-#{inc_proj.id}']"),
+           "Incomplete Project (#{inc_proj.id}) not shown in Open List"
+    assert page.has_xpath?("//*[@id='db-projects-rc']/div"),
+           "Recently Completed Projects not listing"
+    assert page.has_xpath?("//*[@id='db-projects-rc']/div/a"),
+           "Recently Completed Projects has invalid elements"
+
+    assert page.has_xpath?("//*[@id='db-tasks-owned']/div/a[@id='task-#{owned_task.id}']"),
+           "Owned task not rendering"
+    assert page.has_xpath?("//*[@id='db-tasks-reported']/div/a[@id='task-#{reported_task.id}']"),
+           "Reported task not rendering"
+
+    assert_match /John Doe's Dashboard/, page.title,
+                'Page title does not match /#{User.name}\'s Dashboard/'
+  end
 end
